b4f1bf42d678c0affb6612a7d9413a01b684ffd2
Noa r <roliroloyt@gmail.com>
2025-01-14 12:04:20 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Exemple d'interactivité : Défilement fluide vers les sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
